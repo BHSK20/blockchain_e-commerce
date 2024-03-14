6bf8cdddc3bbb805cf295fca2a1b161b25c61c4a
sonkate <tranthanhson03082002@gmail.com>
2024-03-14 23:14:04 +0700

    update db

--- src/apis/register.py
+++ src/apis/register.py
@@ -18,7 +18,8 @@ from src.connect import redis
 from src.lib.roles import Role
 from src.helper.create_wallet import create_wallet
 
-
+from starlette.background import BackgroundTasks
+from starlette.responses import JSONResponse
 class Register(HTTPEndpoint):
 
     @executor(form_data = Register)
@@ -35,29 +36,9 @@ class Register(HTTPEndpoint):
         token = temp.get('token')
         redis.set(form_data['email'], token)
         message = 'Your account has been create successfully, please click the link to verify your account: {}?token={}'.format(config.BASE_URL_TOKEN,token)
-        # task.send_task('worker.send_mail', ("customer_email", form_data['email'], message), queue = 'send_mail')
-        
-        ## register not email, update needed
-        await session.execute(
-            insert(Users).
-            values(email = data['email'], name = data['name'], password = data['password'].encode(), role = Role.USER.value)
-            )
-        await session.commit()
-        await session.close()
-        # create wallet
-        result = await session.execute(select(Users).filter_by(**{'email' : data['email']}))
-        result = result.fetchall()
-        item = result[0]
-        dict_item = item[0].as_dict
-        user_id = dict_item['id']
-        
-        public_key, private_key = create_wallet()
-        key = {'public_key': public_key, 'private_key': private_key}
-        result = await session.execute(insert(Wallet).values(**{'userId' : user_id, 'key' : key}))
-        await session.commit()
-        await session.close()
-        # send_email(form_data['email'], subject='Your account has been created', message=message)
-        return "success"
+        tasks = BackgroundTasks()
+        tasks.add_task(send_email, form_data['email'], subject='Verify your account', message=message)
+        return JSONResponse('success', background=tasks)
 
     @executor(query_params=VerifyAccount)
     async def get(self, query_params):
@@ -67,8 +48,8 @@ class Register(HTTPEndpoint):
         str_data = _decode.get('payload')
         data = json.loads(str_data)
         # check if verified
-        # if await is_exists_email(data['email']):
-        #     return 'already verified'
+        if await is_exists_email(data['email']):
+            return 'already verified'
         stored_token = redis.get(data['email']).decode()
         if stored_token == token:
             await session.execute(
