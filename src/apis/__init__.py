cb75c1375825e59fd98d9a33d7c5b49bd068304b
sonkate <tranthanhson03082002@gmail.com>
2024-03-14 17:57:30 +0700

    update requirements

--- src/apis/__init__.py
+++ src/apis/__init__.py
@@ -3,7 +3,6 @@ from src.apis.health_check import HealthCheck
 from src.apis.login import Login
 from src.apis.logout import Logout
 from src.apis.register import Register
-from src.apis.get_order_input import Checkout
 from src.apis.merchant_register import MerchantRegister
 from src.apis.get_order_input import GetOrderInput
 from src.apis.create_order import CreateOrder
@@ -13,7 +12,6 @@ routes = [
     Route('/login', Login),
     Route('/logout', Logout),
     Route('/register', Register),
-    Route('/checkout', Checkout),
     Route('/merchant_register', MerchantRegister),
     Route('/get_order_input', GetOrderInput),
     Route('/create_order', CreateOrder),
