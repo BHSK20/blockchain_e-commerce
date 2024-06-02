41ef7ca674d25321382371fa64012dbe6b77aa99
sonkate <tranthanhson03082002@gmail.com>
2024-06-02 18:31:04 +0700

    update KYC

--- src/apis/user_info_KYC.py
+++ src/apis/user_info_KYC.py
@@ -1,5 +1,5 @@
 from src.models.user_info import UsersInfo
-from src.connect import session
+from src.connect import session, redis
 from src.models.user_info import UsersInfo
 from sqlalchemy import insert, update
 from starlette.endpoints import HTTPEndpoint
@@ -16,17 +16,34 @@ from src.lib.exception import BadRequest
 class UserInfoKYC(HTTPEndpoint):
     @executor(login_require=login_require)
     async def get(self, user):
+        data = redis.get("KYC_{}".format(user['email']))
+        json_data = json.loads(data) if data else None
+        if json_data:
+            return json_data
         user_info  = await get_user_info_KYC(user['email'])
         return user_info
     @executor(login_require=login_require, form_data=UpdateUserInfo)
     async def post(self, user, form_data):
+        form_data['email'] = user['email']
+        redis.set("KYC_{}".format(form_data['email']), json.dumps(form_data))
+        return form_data
+    
+    @executor(login_require=login_require, form_data=UpdateUserInfo)
+    async def put(self, user, form_data):
+        data = redis.get("KYC_{}".format(user['email']))
+        json_data = json.loads(data) if data else None
+        if not json_data:
+            raise BadRequest('Please update user info first')
+        # compare
+        
+        form_data['email'] = user['email'],
         await session.execute(
             insert(UsersInfo).
             values(
                 form_data,
-                email = user['email'],
                 KYC_check = True,
             ))
         await session.commit()
         await session.close()
+        redis.delete("KYC_{}".format(user['email']))
         return {'status': 'success'}
